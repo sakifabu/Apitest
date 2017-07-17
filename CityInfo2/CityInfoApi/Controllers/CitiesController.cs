ded515c62a8021d4d6b0045b6cc5dceb346479b2
sakifabu <sakifabu@live.com>
2017-07-16 23:41:05 -0400

    Add files via upload

---
+++ CityInfo2/CityInfoApi/Controllers/CitiesController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CityInfoApi.Controllers
+{
+    [Route("api/cities")]
+    public class CitiesController : Controller
+    {
+        [HttpGet()]
+        public IActionResult GetCities()
+        {
+            return Ok(CitiesDataStore.Current.Cities);
+            //return new JsonResult(new List<object>()
+            //{
+            //    new{id =1, Name="New York" },
+            //    new {id= 2, Name ="Lansdale" }
+            //});
+        }
+        [HttpGet("{id}")]
+        public IActionResult GetCity(int id)
+        {
+            var CitytoReturn = CitiesDataStore.Current.Cities.FirstOrDefault(c => c.Id == id);
+            if(CitytoReturn== null)
+            {
+                return NotFound("City Not Found");
+            }
+            return Ok(CitytoReturn);
+        }
+
+    }
+}
---
+++ CityInfo2/CityInfoApi/Models/PointOfInterestDto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CityInfoApi
+{
+    public class PointOfInterestDto
+    {
+        public int Id { get; internal set; }
+        public string Name { get; internal set; }
+        public string Description { get; internal set; }
+    }
+}
